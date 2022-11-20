a8b46be7053064990966c42823d56de829b7a2cb
Rafa Adorna <rafael.adorna@gmail.com>
2022-11-20 01:17:06 +0100

    Update example.

--- examples/main.tf
+++ examples/main.tf
@@ -16,8 +16,6 @@ module "init-environments" {
   for_each              = toset(var.environments)
   source                = "github.com/Thaeimos/terraform-init-envs/?ref=v1.0.0"
   environment           = each.value
-  s3_bucket_prefix      = var.s3_bucket_prefix
-  s3_bucket_name        = var.s3_bucket_name
-  dynamodb_table        = var.dynamodb_table
+  s3_dyn_name           = var.s3_dyn_name
   bucket_sse_algorithm  = var.bucket_sse_algorithm
 }
--- examples/variables.tf
+++ examples/variables.tf
@@ -1,21 +1,11 @@
-variable "environments" {
-  description = "List of environments."
-  type        = list
+variable "environment" {
+  description = "Single environment."
+  type        = string
   nullable    = false
 }
 
-variable "s3_bucket_prefix" {
-  description = "S3 bucket for terraform state."
-  type        = string
-}
-
-variable "s3_bucket_name" {
-  description = "'Name' tag for S3 bucket with terraform state."
-  type        = string
-}
-
-variable "dynamodb_table" {
-  description = "DynamoDB table name for terraform lock."
+variable "s3_dyn_name" {
+  description = "S3 bucket and DynamoDB prefix for names and other attributes."
   type        = string
 }
 
@@ -23,4 +13,10 @@ variable "bucket_sse_algorithm" {
   type        = string
   description = "Encryption algorithm to use on the S3 bucket. Currently only AES256 is supported"
   default     = "AES256"
+}
+
+variable "region" {
+  type        = string
+  description = "Currently mono region. Region where to deploy."
+  default     = "eu-west-2"
 }
